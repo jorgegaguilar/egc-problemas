0acea86108e156a2cc55245f0b5ce7f8e806ffd2
=darkgray <jorgegaguilar@hotmail.es>
2017-11-23 18:53:48 +0100

    Añadido método

--- java-hello/java-hello/src/main/java/hello/GreetingController.java
+++ java-hello/java-hello/src/main/java/hello/GreetingController.java
@@ -15,3 +15,9 @@ public class GreetingController {
     }
 
 }
+
+    @RequestMapping("/bye")
+    public String bye(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "bye";
+    }
